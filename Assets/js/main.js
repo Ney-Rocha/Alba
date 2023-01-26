f6c7efa1c57b6da3ae532f2fe6847e4fbc94874f
Ney Rocha <neyrocha.brasil@gmail.com>
2023-01-26 13:13:39 -0300

    correções de js e button home

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -5,26 +5,14 @@ $(document).ready(function(){
     })
 
     $('#sidebar_menu a').on('click', function (e) {
-        e.preventDefault();
+        
         if ($(window).width() < 575) {
+            e.preventDefault();
             $('#sidebar').removeClass("show");
         }
     });
 
     //  chosen plugin
-    $(".chosen-select").chosen({disable_search_threshold: 10});
+    $(".chosen-select").chosen({disable_search_threshold: 2});
 });
 
-//  SWIRCHERY
-// var elem = document.querySelector('.js-switch');
-// var switchery = new Switchery(elem, { color: '#00A651' });
-
-var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
-
-elems.forEach(function(html) {
-    var switchery = new Switchery(html,  {color: '#00A651' });
-});
-
-function goBack() {
-    window.history.back()
-}
---
+++ Assets/js/perfilgrupo.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function(){
+    //troca de tipo de perfil (uniforme/ Escalonável)
+    $('ul.type-profile li a').click(function(i){
+        $(this).parent().siblings().children().removeClass('active');
+        $(this).parent().parent().parent().siblings().children().removeClass('active');
+        $(this).addClass('active');
+        var id = $(this).data('id');    
+        var content = $(this).parent().parent().parent().siblings().children().filter(function() { 
+            return $(this).data("id") == id 
+        });    
+        content.addClass('active');
+    });
+     
+});
+
+s

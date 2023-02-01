012aa92cef9f65c7a2aa85df17ed3507f78d2a64
Ney Rocha <neyrocha.brasil@gmail.com>
2023-02-01 00:38:44 -0300

    inclusão do plugin print PDF

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -2,6 +2,8 @@
 $(document).ready(function(){
     $("#sidebar_control").on("click", function(){
         $("#sidebar").toggleClass("show")
+        window.scrollTo(0, 0);
+       
     })
 
     $('#sidebar_menu a').on('click', function (e) {

ed1ed3ddc05cb316fdd21edf21f07161c20b7172
Ney Rocha <neyrocha.brasil@gmail.com>
2023-03-09 19:05:21 -0300

    final alterações de lsita pendencias, status proposta e print apolice

--- Assets/js/apolice.js
+++ Assets/js/apolice.js
@@ -2,8 +2,6 @@ $(document).ready(function(){
 
     $('#Download_PDF').on("click", function(){
         $('#Download_PDF').html("<span class='mr-2'><i class='fa fa-spinner fa-pulse'></i></span > carregando")
-        // var activeNav = $('.sidebar-nav-item.active').find('a')
-        // $('#resultContainer').children().removeClass('hide')
         var user = "Marcos";
         var date = new Date().toLocaleDateString();
         $('#resultContainer').print({
@@ -30,6 +28,44 @@ $(document).ready(function(){
             append :"<div class='text-print'><div class='mb-4 mt-4'><span class='mr-1 '><i class='fas fa-exclamation-circle'></i></span> Este documento foi gerado pelo usuário <strong>" + user +"</strong> em <strong>" + date +"</strong> através da ferramenta <strong>Alba&copy;</strong> e só poderá ser utilizada para as devidas atividades de acordo com termo de aceitação de uso e termo de proteção e privacidade de dados, deixando este usuário com total responsabilidade sobre as devidas precauções de proteção, sob as penas previstas no CONTRATO e nas legislações aplicáveis.<div class='content-brand-print'><div class='brand-print'><img src='Assets/img/brand/Logo-Alba-Cabecalho-Site-768x360.png' class='responsive_img'></div></div> ",
         });
         $('#Download_PDF').html("<span class='mr-2'><i class='fa fa-file-download'aria-hidden='true'></i></span > Baixar PDF")
+    })
+
+    $('#assFisica').on("click", function(e){
+        e.preventDefault();
+        $('#assFisica').html("<span class='mr-2'><i class='fa fa-spinner fa-pulse'></i></span > carregando")
+        var user = "Marcos";
+        var date = new Date().toLocaleDateString();
+        $('#resultContainer').print({
+            addGlobalStyles : false,
+            mediaPrint: {
+                // Defina as propriedades de cabeçalho e rodapé
+                    '@page': {
+                    margin: "30mm",
+                    size: "auto",
+                    // Adicione o cabeçalho e o rodapé em cada página
+                    'top': {
+                        content: '<div class="cabecalho">Cabeçalho da página</div>'
+                    },
+                    'bottom': {
+                        content: '<div class="rodape">Rodapé da página</div>'
+                    }
+                }
+            },
+            stylesheet : 'Assets/css/print.css',
+            rejectWindow : false,
+            noPrintSelector : ".no-print",
+            iframe : true,                   
+            prepend :"<div class='text-print'><div class='content-brand-print'><div class='brand-print'><img src='Assets/img/brand/Logo-Alba-Cabecalho-Site-768x360.png' class='responsive_img'></div></div><div class='mb-4'><span class='mr-1 '><i class='fas fa-exclamation-circle'></i></span> Este documento foi gerado pelo usuário <strong>" + user +"</strong> em <strong>" + date +"</strong> através da ferramenta <strong>Alba&copy;</strong> e só poderá ser utilizada para as devidas atividades de acordo com termo de aceitação de uso e termo de proteção e privacidade de dados, deixando este usuário com total responsabilidade sobre as devidas precauções de proteção, sob as penas previstas no CONTRATO e nas legislações aplicáveis.</div></div></div> ",
+            append :"<div class='text-print'><div class='mb-4 mt-4'><span class='mr-1 '><i class='fas fa-exclamation-circle'></i></span> Este documento foi gerado pelo usuário <strong>" + user +"</strong> em <strong>" + date +"</strong> através da ferramenta <strong>Alba&copy;</strong> e só poderá ser utilizada para as devidas atividades de acordo com termo de aceitação de uso e termo de proteção e privacidade de dados, deixando este usuário com total responsabilidade sobre as devidas precauções de proteção, sob as penas previstas no CONTRATO e nas legislações aplicáveis.<div class='content-brand-print'><div class='brand-print'><img src='Assets/img/brand/Logo-Alba-Cabecalho-Site-768x360.png' class='responsive_img'></div></div> ",
+            //deffered callback
+            deferred: $.Deferred().done(function() { 
+                window.location.href = '/anexarDocumentos.html';
+             })
+            
+        });
+
+        $('#assFisica').html(" Assinatura Fisíca <span class='fas fa-file-signature ml-2 '></span>")
+        
     })
 
        
---
+++ Assets/js/statusProposta.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    if($("#protFinalizado").hasClass("active")){
+        $("#iconFinish").addClass("show")
+    }else{
+        $("#iconFinish").removeClass("show")
+    }
+});
